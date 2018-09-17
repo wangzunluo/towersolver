792659abf7cde3deeb487511713b3a58b12a410b
Kevin Jiang <kjiang1998@gmail.com>
2018-09-17 15:07:23 -0500

    added some contour recognition

---
+++ recognize_digits.py
@@ -0,0 +1,53 @@
+from imutils import contours
+from imutils.perspective import four_point_transform
+import imutils
+import cv2
+from matplotlib import pyplot as plt
+
+# define the dictionary of digit segments so we can identify
+# each digit on the thermostat
+DIGITS_LOOKUP = {
+    (1, 1, 1, 0, 1, 1, 1): 0,
+    (0, 0, 1, 0, 0, 1, 0): 1,
+    (1, 0, 1, 1, 1, 1, 0): 2,
+    (1, 0, 1, 1, 0, 1, 1): 3,
+    (0, 1, 1, 1, 0, 1, 0): 4,
+    (1, 1, 0, 1, 0, 1, 1): 5,
+    (1, 1, 0, 1, 1, 1, 1): 6,
+    (1, 0, 1, 0, 0, 1, 0): 7,
+    (1, 1, 1, 1, 1, 1, 1): 8,
+    (1, 1, 1, 1, 0, 1, 1): 9
+}
+
+# load the example image
+image = cv2.imread("tower.png")
+
+# pre-process the image by resizing it, converting it to grayscale, blurring it, and computing an edge map
+image = imutils.resize(image, height=500)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(blurred, 50, 200, 255)
+
+# find contours in the edge map, then sort them by their size in descending order
+cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+                        cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+displayCnt = None
+
+# loop over the contours
+for c in cnts:
+    # approximate the contour
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+    # if the contour has four vertices, then we have found the thermostat display
+    if len(approx) == 4:
+        displayCnt = approx
+        break
+
+warped = four_point_transform(gray, displayCnt.reshape(4, 2))
+output = four_point_transform(image, displayCnt.reshape(4, 2))
+
+plt.imshow(output)
+plt.show()
