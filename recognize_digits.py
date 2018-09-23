ae4429e8f341cda3c406b5ab137091451accd7b2
Kevin Jiang <kjiang1998@gmail.com>
2018-09-22 23:19:14 -0500

    added code to crop image to include only the puzzle

--- recognize_digits.py
+++ recognize_digits.py
@@ -20,11 +20,11 @@ DIGITS_LOOKUP = {
 }
 
 # load the example image
-image = cv2.imread("tower.png")
+img = cv2.imread("tower.png")
 
 # pre-process the image by resizing it, converting it to grayscale, blurring it, and computing an edge map
-image = imutils.resize(image, height=500)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = imutils.resize(img, height=500)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 edged = cv2.Canny(blurred, 50, 200, 255)
 
@@ -34,6 +34,7 @@ cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
 cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
 displayCnt = None
+
 
 # loop over the contours
 for c in cnts:
@@ -46,8 +47,28 @@ for c in cnts:
         displayCnt = approx
         break
 
-warped = four_point_transform(gray, displayCnt.reshape(4, 2))
-output = four_point_transform(image, displayCnt.reshape(4, 2))
+buffer = 30
+x,y,w,h = cv2.boundingRect(displayCnt)
+x -= buffer
+y -= buffer
+w += 2*buffer
+h += 2*buffer
+cropped_img = gray[y:y+h, x:x+h]
 
-plt.imshow(output)
+thresh = cv2.threshold(cropped_img, 0, 255,
+    cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+# kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
+# thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+    cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+digitsCnts = []
+
+for c in cnts:
+    # compute the bounding box of the contour
+    (x, y, h, w) = cv2.boundingRect(c)
+    cv2.rectangle(thresh, (x,y), (x+w,y+h),(0.255,0),2)
+
+plt.imshow(thresh)
 plt.show()
